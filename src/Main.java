2c90a221af1477b3f2aa2565f3846713ace790e4
CODIPHENIX <bwangoastrid@gmail.com>
2024-07-05 17:41:40 +0200

    nested

--- src/Main.java
+++ src/Main.java
@@ -5,18 +5,36 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+        int row=0;
+        int col=0;
+        String symbol= "";
 
-        System.out.println("what is your age");
-        int age = scanner.nextInt();
+        while (row<=0){
+            System.out.println("enter the number of row");
+            row = scanner.nextInt();
+        }
+        while (col<=0) {
+            System.out.println("enter the number of column");
+            col = scanner.nextInt();
+        }
+        while (symbol.isBlank() || symbol.length()>1) {
+            System.out.println("enter the a symbol");
+            symbol = scanner.next();
+        }
 
-        if(age>=75){
-            System.out.println("you are a boommer");
-        }else if(age>=18){
-            System.out.println("you are an adult");
-        } else if (age>=13) {
-            System.out.println("your are a teeneger");
-        } else {
-            System.out.println("You a still a child");
+
+            for ( int i= 1; i<=row; i++)
+            {
+                for ( int j = 1;j <= col; j++)
+                {
+                    if (i==1 || i == row || j==1|| j == col ) {
+                        System.out.print(symbol);
+                    } else {
+                        System.out.print(" ");
+                    }
+                }
+                System.out.println();
+            }
+            System.out.println("happy new year");
         }
     }
-}
